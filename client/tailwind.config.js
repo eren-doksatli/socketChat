4bba72cfcfb784f88d16fae854acd44bd016977b
eren-doksatli <eren.doksatli@gmail.com>
2024-11-10 20:32:17 +0300

    Removed client submodule and added client folder

---
+++ client/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // React bileşenlerinin bulunduğu dosyaları taramak için
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
